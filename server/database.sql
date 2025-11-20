71d7aa7f96d8e076b6a62aff708aebe0043d74f5
OspinaJuan <ospinaangaritaoa@gmail.com>
2025-11-19 21:02:42 -0500

    Created post route for profiles

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE languageapp;
 
-CREATE TABLE profile(
+CREATE TABLE profiles(
 	profile_id SERIAL PRIMARY KEY,
 	native_language VARCHAR(50),
 	target_language VARCHAR(50)
--- server/index.js
+++ server/index.js
@@ -9,13 +9,23 @@ app.use(express.json());
 
 // ROUTES //
 
-app.post("/profile", async(req,res) => {
+app.post("/profile", async(req, res) => {
 	try {
-		
+		const { native_language, target_language } = req.body;
+		console.log(target_language);
+		const newProfile = await pool.query(
+			`INSERT INTO profiles (native_language, target_language) VALUES ($1, $2) RETURNING *`,
+			[native_language, target_language]
+		);
+
+		res.json(newProfile.rows[0]);
 	} catch (err) {
 		console.error(err.message);
+		res.status(500).send("Server error");
 	}
-})
+});
+
+
 
 app.listen(5000, () => {
 	console.log("Server has started on port 5000")
